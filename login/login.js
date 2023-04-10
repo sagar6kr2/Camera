adce31a83de8cb1f4f19dae4d1385d5cd9e0c24f
sagar6kr2 <126499479+sagar6kr2@users.noreply.github.com>
2023-04-11 02:01:53 +0530

    commit

---
+++ login/login.js
@@ -0,0 +1,25 @@
+const login = document.getElementById('login');
+
+
+
+login.onclick = (e) => {
+   let reval = true;
+   e.preventDefault();
+   const EmailAddress = document.getElementById('femail').value;
+   const Password = document.getElementById('fpass').value;
+   const Emails = localStorage.getItem('Email');
+   const Passwords = localStorage.getItem('Password');
+
+   if (EmailAddress != Emails) {
+      alert("Wrong EmailAddress");
+      return false;
+   }
+   if (Password != Passwords) {
+      alert("Wrong Password");
+      return false;
+   }
+   else {
+      alert('great job');
+   }
+   return reval;
+}
---
+++ login/registration.js
@@ -0,0 +1,42 @@
+const btn = document.getElementById('btns');
+
+
+btn.onclick = () => {
+
+  let revalue = true;
+
+  const name = document.getElementById('fname').value;
+  if (name.length < 10) {
+    alert("name length is too short");
+    return false
+  }
+  localStorage.setItem('Name', name);
+
+  const email = document.getElementById('femail').value;
+  if (email.length < 7) {
+    alert("please check your email");
+    return false;
+  }
+  localStorage.setItem('Email', email);
+
+  const number = document.getElementById('fnumber').value;
+  if (number.length < 10) {
+    alert("please enter  valide number");
+    return false;
+  }
+  localStorage.setItem('Number', number);
+
+  const password = document.getElementById('fpass').value;
+  const cpassword = document.getElementById('fcpass').value;
+  localStorage.setItem('Password', password);
+  localStorage.setItem('confirmPassword', cpassword);
+  if (password != cpassword) {
+    alert("wrong Password");
+    return false;
+  }
+  else {
+    alert('good job')
+  }
+  return revalue;
+}
+
